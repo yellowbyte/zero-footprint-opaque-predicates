cd7bcee26fee59f7bf80864f40971969193ee487
Yu-Jye Tung <yu.jye.tung@gmail.com>
2021-08-29 22:51:42 -0700

    Update prettyvsa.ml to ignore unsigned variables since they mess with our opaque predicates tool

--- installs/frama-c/prettyvsa.ml
+++ installs/frama-c/prettyvsa.ml
@@ -12,18 +12,38 @@ let contains s1 s2 =
 
 
 (* Prints the value associated to a pointer *)
-let pretty_lval fmt stmt lval =
+let pretty_lval fmt stmt lval vi =
   let kinstr = Kstmt stmt in (* Make a kinstr from a stmt *)
   let loc = (* Make a location from a kinstr + an lval *)
     !Db.Value.lval_to_loc kinstr ~with_alarms:CilE.warn_none_mode lval
   in
-  Db.Value.fold_state_callstack
-    (fun state () ->
-       (* For each state in the callstack *)
-       let value = Db.Value.find state loc in (* Obtain value for location *)
-       Format.fprintf fmt "\"%a:%a\",@." Printer.pp_lval lval
-         Locations.Location_Bytes.pretty value (* Print mapping *)
-    ) () ~after:true kinstr (* Get value set after execution of statement  *)
+  match vi.vtype with
+  | TInt (ikind,_) -> begin
+      match ikind with 
+      | IUInt | IUShort | IUChar | IULong | IULongLong -> 
+        (* Ignore unsigned variable since it messes with the opaque predicate tool  *)
+        ()
+      | _ -> begin
+        Db.Value.fold_state_callstack
+          (fun state () ->
+             (* For each state in the callstack *)
+             let value = Db.Value.find state loc in (* obtain value for location *)
+             (* Print variable name and corresponding value set *)
+             Format.fprintf fmt "\"%a:%a\",@." Printer.pp_lval lval
+               Locations.Location_Bytes.pretty value (* print mapping *)
+          ) () ~after:true kinstr; (* Get value set after execution of statement  *)
+        end
+    end
+  | _ -> begin
+    Db.Value.fold_state_callstack
+      (fun state () ->
+         (* For each state in the callstack *)
+         let value = Db.Value.find state loc in (* obtain value for location *)
+         (* Print variable name and corresponding value set *)
+         Format.fprintf fmt "\"%a:%a\",@." Printer.pp_lval lval
+           Locations.Location_Bytes.pretty value (* print mapping *)
+      ) () ~after:true kinstr; (* Get value set after execution of statement  *)
+    end
 
 
 (* Prints the value associated to variable [vi] before [stmt]. *)
@@ -33,14 +53,33 @@ let pretty_vi fmt stmt vi =
   let loc = (* Make a location from a kinstr + an lval *)
     !Db.Value.lval_to_loc kinstr ~with_alarms:CilE.warn_none_mode lval
   in
-  Db.Value.fold_state_callstack
-    (fun state () ->
-       (* For each state in the callstack *)
-       let value = Db.Value.find state loc in (* obtain value for location *)
-       (* Print variable name and corresponding value set *)
-       Format.fprintf fmt "\"%a:%a\",@." Printer.pp_varinfo vi
-         Locations.Location_Bytes.pretty value (* print mapping *)
-    ) () ~after:true kinstr (* Get value set after execution of statement  *)
+  match vi.vtype with
+  | TInt (ikind,_) -> begin
+      match ikind with 
+      | IUInt | IUShort | IUChar | IULong | IULongLong -> 
+        (* Ignore unsigned variable since it messes with the opaque predicate tool  *)
+        ()
+      | _ -> begin
+        Db.Value.fold_state_callstack
+          (fun state () ->
+             (* For each state in the callstack *)
+             let value = Db.Value.find state loc in (* obtain value for location *)
+             (* Print variable name and corresponding value set *)
+             Format.fprintf fmt "\"%a:%a\",@." Printer.pp_varinfo vi
+               Locations.Location_Bytes.pretty value (* print mapping *)
+          ) () ~after:true kinstr; (* Get value set after execution of statement  *)
+        end
+    end
+  | _ -> begin
+    Db.Value.fold_state_callstack
+      (fun state () ->
+         (* For each state in the callstack *)
+         let value = Db.Value.find state loc in (* obtain value for location *)
+         (* Print variable name and corresponding value set *)
+         Format.fprintf fmt "\"%a:%a\",@." Printer.pp_varinfo vi
+           Locations.Location_Bytes.pretty value (* print mapping *)
+      ) () ~after:true kinstr; (* Get value set after execution of statement  *)
+    end
 
 
 (* Prints the state at statement [stmt] for each local variable in [kf],
@@ -53,7 +92,7 @@ let pretty_local_and_global_vars kf fmt stmt =
     if Cil.isPointerType vi.vtype then
       (* Variable is a pointer. Print it as such so user knows *)
       let lval = (Mem (Cil.evar vi), NoOffset) in
-      pretty_lval fmt stmt lval
+      pretty_lval fmt stmt lval vi
     else if Cil.isArrayType vi.vtype then
       (* Defined array. Skip *)
       ()
@@ -69,7 +108,7 @@ let pretty_local_and_global_vars kf fmt stmt =
       if Cil.isPointerType vi.vtype then
         (* Variable is a pointer. Print it as such so user knows *)
         let lval = (Mem (Cil.evar vi), NoOffset) in
-        pretty_lval fmt stmt lval
+        pretty_lval fmt stmt lval vi
       else if Cil.isArrayType vi.vtype then
         (* Defined array. Skip *)
         ()
